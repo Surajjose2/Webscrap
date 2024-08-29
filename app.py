fca06a0db56d40ed7564abaa6824f919c55133c7
Surajjose2 <surajjosep123asd@gmail.com>
2024-08-29 10:37:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,29 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 def get_all_urls(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Check for HTTP errors
+  try:
+    response = requests.get(url)
+    response.raise_for_status() 
+    soup = BeautifulSoup(response.content, 'html.parser')
+    urls = []
+    for link in soup.find_all('a'):
+      href = link.get('href')
+      if href:
+        urls.append(href)
+    return urls
+  except requests.exceptions.RequestException as e:
+    print(f"An error occurred: {e}")
+    return []
 
-        soup = BeautifulSoup(response.content, 'html.parser')
-
-        urls = []
-        for link in soup.find_all('a'):
-            href = link.get('href')
-            if href:
-                urls.append(href)
-
-        return urls
-
-    except requests.exceptions.RequestException as e:
-        print(f"An error occurred: {e}")
-        return []  # Return an empty list in case of errors
-
-# Example usage
-target_website = "https://www.indianyellowpages.com/ahmedabad"  # Replace with the website you want to scrape
+# Example Usage
+target_website = "https://www.indianyellowpages.com"
 all_urls = get_all_urls(target_website)
 
 if all_urls:
-    print("URLs found:")
-    for url in all_urls:
-        print(url)
+  df = pd.DataFrame(all_urls, columns=['URLs'])
+  df.to_excel("extracted_urls.xlsx", index=False)
+  print("URLs extracted and saved to 'extracted_urls.xlsx'")
 else:
-    print("No URLs found or an error occurred.")
+  print("No URLs found or an error occurred.")
